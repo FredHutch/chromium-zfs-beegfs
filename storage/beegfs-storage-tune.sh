2a6f65aa9f3238847e6eda79cd18f48422027565
Jeff Katcher <jmkatcher@yahoo.com>
2017-08-10 12:33:35 -0700

    made drive for loops more readable

--- storage/beegfs-storage-tune.sh
+++ storage/beegfs-storage-tune.sh
@@ -4,17 +4,29 @@
 #          Assumes drive configuration == chromium-store[5-8]!
 # ------------------------------------------------------------------
 #
-# Set an appropriate IO scheduler for file servers
-for drive in {a..z}; do echo deadline >/sys/block/sd$drive/queue/scheduler; done
-for drive in {a..h}; do echo deadline >/sys/block/sda$drive/queue/scheduler; done
-#
-# Increase IO scheduler number of schedulable requests
-for drive in {a..z}; do echo 4096 > /sys/block/sd$drive/queue/nr_requests; done
-for drive in {a..h}; do echo 4096 > /sys/block/sda$drive/queue/nr_requests; done
-#
-# Improve sequential read throughput by increasing maximum read-ahead
-for drive in {a..z}; do echo 4096 > /sys/block/sd$drive/queue/read_ahead_kb; done
-for drive in {a..h}; do echo 4096 > /sys/block/sda$drive/queue/read_ahead_kb; done
+for drive in {a..z}
+do
+	# Set an appropriate IO scheduler for file servers
+	echo deadline >/sys/block/sd$drive/queue/scheduler
+
+	# Increase IO scheduler number of schedulable requests
+	echo 4096 > /sys/block/sd$drive/queue/nr_requests
+
+	# Improve sequential read throughput by increasing maximum read-ahead
+	echo 4096 > /sys/block/sd$drive/queue/read_ahead_kb
+done
+	
+for drive in {a..h}
+do
+	# Set an appropriate IO scheduler for file servers
+	echo deadline >/sys/block/sda$drive/queue/scheduler
+
+	# Increase IO scheduler number of schedulable requests
+	echo 4096 > /sys/block/sda$drive/queue/nr_requests
+
+	# Improve sequential read throughput by increasing maximum read-ahead
+	echo 4096 > /sys/block/sda$drive/queue/read_ahead_kb
+done
 #
 # Avoid long IO stalls (latencies) for write cache flushing
 echo 5 > /proc/sys/vm/dirty_background_ratio
